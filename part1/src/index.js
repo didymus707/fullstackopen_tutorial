2a60cb9e0cb64406cc3ed23e39ba683bc614d6c0
Adewale Orotayo <didymus7007@gmail.com>
2020-12-15 11:00:11 +0100

    Part1 tutorial

---
+++ part1/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Tobih';
+  const age = 27;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Shalom" age={10 + 17}/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
